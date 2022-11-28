e422d927060ed9244fc3cc735458dac05d600853
mohammed hassan <46082551+made112@users.noreply.github.com>
2022-11-28 09:10:49 +0200

    =first comment

---
+++ resources/views/base/base.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+{{--
+Product Name: {{ theme()->getOption('product', 'description') }}
+Author: KeenThemes
+Purchase: {{ theme()->getOption('product', 'purchase') }}
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+License: {{ theme()->getOption('product', 'license') }}
+--}}
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"{!! theme()->printHtmlAttributes('html') !!} {{ theme()->printHtmlClasses('html') }}>
+{{-- begin::Head --}}
+<head>
+    <meta charset="utf-8"/>
+    <title>{{ ucfirst(theme()->getOption('meta', 'title')) }} | Keenthemes</title>
+    <meta name="description" content="{{ ucfirst(theme()->getOption('meta', 'description')) }}"/>
+    <meta name="keywords" content="{{ theme()->getOption('meta', 'keywords') }}"/>
+    <link rel="canonical" href="{{ ucfirst(theme()->getOption('meta', 'canonical')) }}"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="shortcut icon" href="{{ asset(theme()->getDemo() . '/' .theme()->getOption('assets', 'favicon')) }}"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+    {{-- begin::Fonts --}}
+    {{ theme()->includeFonts() }}
+    {{-- end::Fonts --}}
+
+    @if (theme()->hasOption('page', 'assets/vendors/css'))
+        {{-- begin::Page Vendor Stylesheets(used by this page) --}}
+        @foreach (array_unique(theme()->getOption('page', 'assets/vendors/css')) as $file)
+            {!! preloadCss(assetCustom($file)) !!}
+        @endforeach
+        {{-- end::Page Vendor Stylesheets --}}
+    @endif
+
+    @if (theme()->hasOption('page', 'assets/custom/css'))
+        {{-- begin::Page Custom Stylesheets(used by this page) --}}
+        @foreach (array_unique(theme()->getOption('page', 'assets/custom/css')) as $file)
+            {!! preloadCss(assetCustom($file)) !!}
+        @endforeach
+        {{-- end::Page Custom Stylesheets --}}
+    @endif
+
+    @if (theme()->hasOption('assets', 'css'))
+        {{-- begin::Global Stylesheets Bundle(used by all pages) --}}
+        @foreach (array_unique(theme()->getOption('assets', 'css')) as $file)
+            @if (strpos($file, 'plugins') !== false)
+                {!! preloadCss(assetCustom($file)) !!}
+            @else
+                <link href="{{ assetCustom($file) }}" rel="stylesheet" type="text/css"/>
+            @endif
+        @endforeach
+        {{-- end::Global Stylesheets Bundle --}}
+    @endif
+
+    @if (theme()->getViewMode() === 'preview')
+        {{ theme()->getView('partials/trackers/_ga-general') }}
+        {{ theme()->getView('partials/trackers/_ga-tag-manager-for-head') }}
+    @endif
+
+    @yield('styles')
+</head>
+{{-- end::Head --}}
+
+{{-- begin::Body --}}
+<body {!! theme()->printHtmlAttributes('body') !!} {!! theme()->printHtmlClasses('body') !!} {!! theme()->printCssVariables('body') !!}>
+
+@if (theme()->getOption('layout', 'loader/display') === true)
+    {{ theme()->getView('layout/_loader') }}
+@endif
+
+@yield('content')
+
+{{-- begin::Javascript --}}
+@if (theme()->hasOption('assets', 'js'))
+    {{-- begin::Global Javascript Bundle(used by all pages) --}}
+    @foreach (array_unique(theme()->getOption('assets', 'js')) as $file)
+        <script src="{{ asset(theme()->getDemo() . '/' .$file) }}"></script>
+    @endforeach
+    {{-- end::Global Javascript Bundle --}}
+@endif
+
+@if (theme()->hasOption('page', 'assets/vendors/js'))
+    {{-- begin::Page Vendors Javascript(used by this page) --}}
+    @foreach (array_unique(theme()->getOption('page', 'assets/vendors/js')) as $file)
+        <script src="{{ asset(theme()->getDemo() . '/' .$file) }}"></script>
+    @endforeach
+    {{-- end::Page Vendors Javascript --}}
+@endif
+
+@if (theme()->hasOption('page', 'assets/custom/js'))
+    {{-- begin::Page Custom Javascript(used by this page) --}}
+    @foreach (array_unique(theme()->getOption('page', 'assets/custom/js')) as $file)
+        <script src="{{ asset(theme()->getDemo() . '/' .$file) }}"></script>
+    @endforeach
+    {{-- end::Page Custom Javascript --}}
+@endif
+{{-- end::Javascript --}}
+
+@if (theme()->getViewMode() === 'preview')
+    {{ theme()->getView('partials/trackers/_ga-tag-manager-for-body') }}
+@endif
+
+@yield('scripts')
+<script>var hostUrl = "assets/";</script>
+<!--begin::Global Javascript Bundle(used by all pages)-->
+<script src="{{asset('assets/plugins/global/plugins.bundle.js')}}"></script>
+<script src="{{asset('assets/js/scripts.bundle.js')}}"></script>
+<!--end::Global Javascript Bundle-->
+<!--begin::Page Vendors Javascript(used by this page)-->
+<script src="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js')}}"></script>
+<script src="{{asset('assets/plugins/custom/datatables/datatables.bundle.js')}}"></script>
+<!--end::Page Vendors Javascript-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="{{asset('assets/js/widgets.bundle.js')}}"></script>
+<script src="{{asset('assets/js/custom/widgets.js')}}"></script>
+<script src="{{asset('assets/js/custom/apps/chat/chat.js')}}"></script>
+<script src="{{asset('assets/js/custom/utilities/modals/upgrade-plan.js')}}"></script>
+<script src="{{asset('assets/js/custom/utilities/modals/create-app.js')}}"></script>
+<script src="{{asset('assets/js/custom/utilities/modals/users-search.js')}}"></script>
+
+</body>
+{{-- end::Body --}}
+
+</html>
