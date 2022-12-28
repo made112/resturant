e67da11f6b5f9db74688e742bac28e44938ebf66
mahmoudkeshta <mahmoudkeshta1@gmail.com>
2022-12-28 19:19:08 +0200

    add css

--- resources/views/site/From.blade.php
+++ resources/views/site/From.blade.php
@@ -13,17 +13,17 @@
     <title>Au Register Forms by Colorlib</title>
 
     <!-- Icons font CSS-->
-    <link href="{{ asset('vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
+    <link href="assets/formcss/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
     <!-- Font special for pages-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 
     <!-- Vendor CSS-->
-    <link href="{{ asset('vendor/select2/select2.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/datepicker/daterangepicker.css') }}" rel="stylesheet" media="all">
+    <link href="../../vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="../../vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet" media="all">
+    <link href="../../css/main.css" rel="stylesheet" media="all">
 </head>
 
 <body>
@@ -136,11 +136,11 @@
     </div>
 
     <!-- Jquery JS-->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
     <!-- Vendor JS-->
-    <script src="{{ asset('vendor/select2/select2.min.js') }}"></script>
-    <script src="{{ asset('vendor/datepicker/moment.min.js') }}"></script>
-    <script src="{{ asset('vendor/datepicker/daterangepicker.js') }}"></script>
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
 
     <!-- Main JS-->
     <script src="js/global.js"></script>
